5c7a09bf5ecbafed66b74731e9c438d5f7901cc4
Nastassia Martin <nastassia.martin@googlemail.com>
2023-12-19 10:41:15 +0100

    set up project and install scss, firebase, react-hook-form, react-router,zod and netlify

---
+++ Take-Care/src/App.tsx
@@ -0,0 +1,15 @@
+//import "./App.css";
+/**
+ * @todo - set up routing for dashboard (teacher)
+ *
+ *
+ */
+const App = () => {
+  return (
+    <>
+      <h1>Take Care</h1>
+    </>
+  );
+};
+
+export default App;
