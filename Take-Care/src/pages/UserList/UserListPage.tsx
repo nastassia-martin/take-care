f0f76dadedc71a12b721c991d7589165c15fa3e4
Nastassia Martin <nastassia.martin@googlemail.com>
2024-01-16 10:47:36 +0100

    update profile pic click will direct to parent profile

--- Take-Care/src/pages/UserList/UserListPage.tsx
+++ Take-Care/src/pages/UserList/UserListPage.tsx
@@ -13,87 +13,7 @@ import { useEffect, useState } from "react";
 import Button from "../../components/Button/Button";
 import { updateParentRole } from "../../helpers";
 import { Role } from "../../types/GenericTypes.types";
-
-const handleParentRoleClick = async (parentId: string, role: Role) => {
-  const newRole = role === Role.NotApproved ? Role.User : Role.NotApproved;
-  // if role is User, then isAuthorizedForPickUp is also true
-  const isAuthorizedForPickUp = newRole === Role.User;
-
-  try {
-    await updateParentRole(parentId, newRole, isAuthorizedForPickUp);
-  } catch (error: any) {
-    <Alert variant="warning">{error.message}</Alert>;
-  }
-};
-
-const columns: ColumnDef<ParentProfile>[] = [
-  {
-    header: "Parent Information",
-    columns: [
-      {
-        header: "Profile pic",
-        accessorKey: "contact.photoURL",
-        cell: ({ row }) => {
-          return (
-            <div className="flex items-center">
-              <Image
-                className="rounded-circle rounded mx-auto d-block"
-                src={row.original.contact.photoURL}
-                alt={`${row.original.contact.firstName} ${row.original.contact.lastName}`}
-                width={40}
-              />
-            </div>
-          );
-        },
-      },
-      {
-        header: "First name",
-        accessorKey: "contact.firstName",
-      },
-      {
-        header: "Last name",
-        accessorKey: "contact.lastName",
-      },
-      {
-        header: "Status",
-        accessorKey: "role",
-      },
-      {
-        header: "Change status",
-        accessorKey: "_id",
-        meta: {
-          align: "center",
-        },
-        cell: (cell) => (
-          <Button
-            ariaLabel="change parent status"
-            onClick={() =>
-              handleParentRoleClick(
-                cell.row.original._id,
-                cell.row.original.role
-              )
-            }
-          >
-            Change status
-          </Button>
-        ),
-      },
-    ],
-  },
-  {
-    header: "Child information",
-    columns: [
-      {
-        header: "First name",
-        accessorKey: "childrenContact.firstName",
-      },
-      {
-        header: "Last name",
-        accessorKey: "childrenContact.lastName",
-      },
-    ],
-  },
-];
+import { useNavigate } from "react-router-dom";
 
 const UserListPage = () => {
   const { currentUser } = useAuth();
@@ -106,6 +26,7 @@ const UserListPage = () => {
     useGetParentsForAdminList();
 
   const isLoading = teacherLoading || parentsLoading;
+  const navigate = useNavigate();
 
   useEffect(() => {
     // When teacher data is fetched, check if the role is 'admin'
@@ -123,6 +44,93 @@ const UserListPage = () => {
   if (!currentUser) {
     return <div>You don't have a profile.</div>;
   }
+  const handleParentRoleClick = async (parentId: string, role: Role) => {
+    const newRole = role === Role.NotApproved ? Role.User : Role.NotApproved;
+    // if role is User, then isAuthorizedForPickUp is also true
+    const isAuthorizedForPickUp = newRole === Role.User;
+
+    try {
+      await updateParentRole(parentId, newRole, isAuthorizedForPickUp);
+    } catch (error: any) {
+      <Alert variant="warning">{error.message}</Alert>;
+    }
+  };
+
+  const handleGoToProfile = (id: string) => {
+    navigate(`/parents/${id}`);
+  };
+
+  const columns: ColumnDef<ParentProfile>[] = [
+    {
+      header: "Parent Information",
+      columns: [
+        {
+          header: "Profile pic",
+          accessorKey: "contact.photoURL",
+          cell: ({ row }) => {
+            return (
+              <div
+                className="flex items-center"
+                onClick={() => handleGoToProfile(row.original._id)}
+              >
+                <Image
+                  className="rounded-circle rounded mx-auto d-block"
+                  src={row.original.contact.photoURL}
+                  alt={`${row.original.contact.firstName} ${row.original.contact.lastName}`}
+                  width={40}
+                />
+              </div>
+            );
+          },
+        },
+        {
+          header: "First name",
+          accessorKey: "contact.firstName",
+        },
+        {
+          header: "Last name",
+          accessorKey: "contact.lastName",
+        },
+        {
+          header: "Status",
+          accessorKey: "role",
+        },
+        {
+          header: "Change status",
+          accessorKey: "_id",
+          meta: {
+            align: "center",
+          },
+          cell: (cell) => (
+            <Button
+              ariaLabel="change parent status"
+              onClick={() =>
+                handleParentRoleClick(
+                  cell.row.original._id,
+                  cell.row.original.role
+                )
+              }
+            >
+              Change status
+            </Button>
+          ),
+        },
+      ],
+    },
+    {
+      header: "Child information",
+      columns: [
+        {
+          header: "First name",
+          accessorKey: "childrenContact.firstName",
+        },
+        {
+          header: "Last name",
+          accessorKey: "childrenContact.lastName",
+        },
+      ],
+    },
+  ];
 
   return (
     <main className={styles.PageWrapper}>
